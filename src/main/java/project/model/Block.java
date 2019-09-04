1dd97392aad98d0da901e40db2d5e08f165df83e
Fredy Ramirez <fredyjr0@gmail.com>
2019-09-03 23:56:49 -0400

    Changed database URL in order to work for all computers, added to Column and Block entity classes

--- src/main/java/project/model/Block.java
+++ src/main/java/project/model/Block.java
@@ -1,5 +1,58 @@
 package project.model;
 
-public class Block {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity(name = "Block")
+public class Block {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long blockId;
+	private Long value;
+	
+	@ManyToOne
+	@JoinColumn(name = "colId")
+	private Long colId;
+	
+	@ManyToOne
+	@JoinColumn(name = "resId")
+	private Long resId;
+	
+	public Long getBlockId() {
+		return blockId;
+	}
+	
+	public void setBlockId(Long blockId) {
+		this.blockId = blockId;
+	}
+	
+	public Long getValue() {
+		return value;
+	}
+	
+	public void setValue(Long value) {
+		this.value = value;
+	}
+	
+	public Long getColId() {
+		return colId;
+	}
+	
+	public void setColId(Long colId) {
+		this.colId = colId;
+	}
+	
+	public Long getResId() {
+		return resId;
+	}
+	
+	public void setResId(Long resId) {
+		this.resId = resId;
+	}
+	
 }
--- src/main/java/project/model/Column.java
+++ src/main/java/project/model/Column.java
@@ -1,5 +1,55 @@
 package project.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "Column")
 public class Column {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long colId;
+	private String colName;
+	private String type;
+	
+	@ManyToOne
+	@JoinColumn(name = "projId")
+	private Long projId;
+	
+	public Long getColId() {
+		return colId;
+	}
+	
+	public void setColId(Long colId) {
+		this.colId = colId;
+	}
+	
+	public String getColName() {
+		return colName;
+	}
+	
+	public void setColName(String colName) {
+		this.colName = colName;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public Long getProjId() {
+		return projId;
+	}
+	
+	public void setProjId(Long projId) {
+		this.projId = projId;
+	}
+	
 }
