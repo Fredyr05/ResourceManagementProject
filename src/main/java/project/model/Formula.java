e4e8445cf0a88682b0ea5c3e683c37b5504c6d0b
Fredy Ramirez <fredyjr0@gmail.com>
2019-09-05 14:06:36 -0400

    Fixed up Formula Foreign Key relationship

--- src/main/java/project/model/Formula.java
+++ src/main/java/project/model/Formula.java
@@ -4,9 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
 public class Formula {
@@ -16,9 +14,8 @@ public class Formula {
 	private Long id;
 	
 	@OneToOne
-	@JoinColumn(name="colId")
-	@PrimaryKeyJoinColumn
 	private Columns columns;
+	
 	private String equation;
 	
 	public Long getFormulaId() {
