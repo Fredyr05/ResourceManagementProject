c2653ad54c421ca5331c1046c4e11dbc7b4eb5aa
Fredy Ramirez <fredyjr0@gmail.com>
2019-09-17 23:10:45 -0400

    Added CORS availability

--- src/main/java/project/config/WebConfig.java
+++ src/main/java/project/config/WebConfig.java
@@ -2,6 +2,7 @@ package project.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,4 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan(basePackages = { "project.controller" })
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+	
 }
